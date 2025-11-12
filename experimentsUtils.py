b5c8aa104290a55fa893c82c300453188fac3802
inbalh1 <inbal12343@gmail.com>
2025-11-12 21:55:04 +0200

    Successfully import pygirgs

--- experimentsUtils.py
+++ experimentsUtils.py
@@ -8,12 +8,9 @@ def setup(is_colab=False):
     # Try using more than 1 core
     run.use_cores(max(1, multiprocessing.cpu_count() - 2))
 
-    # For google colab
+    # For google colab - to import pygirgs.
+
     if is_colab:
-        import importlib.util
-        so_path = 'src/pygirgs/pygirgs.cpython-311-x86_64-linux-gnu.so'
-        spec = importlib.util.spec_from_file_location("pygirgs", so_path)
-        pygirgs = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(pygirgs)
-        
+        import os
+        os.environ["PYTHONPATH"] = os.path.abspath("src/pygirgs")
 
